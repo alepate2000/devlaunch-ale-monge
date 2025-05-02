00c286740bd714fa7ac7427051e466e6bab24727
Alejandro Monge <alemonge2000@hotmail.com>
2025-05-02 12:08:45 -0600

    chore(variables) supermarket homework

---
+++ fundamentals/variables/supermarket.js
@@ -0,0 +1,15 @@
+/*
+Vot al supermercado GreenCenter para hacer mis compras habituales y planeo comprar manzanas y naranjas.
+Cuando estoy ahi, veo que no hay platanos ni uvas, asi que cambio un poco mi lista.
+Veo que hay 3 cajeros están trabajando, asi que me preparo para pagar.
+Mis cosas cestan $100 en total.
+Entrego $150 en efectivo y recibo $50 de cambio.
+*/
+
+const marketName = 'GreenCenter'
+
+const favouriteFruit1 = 'manzanas'
+const favouriteFruit2 = 'naranjas'
+
+const areBananasAvailable = false
+const areGrapesAvailable = false 
