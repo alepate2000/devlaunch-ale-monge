f4337d43310051f4fa3cc66c5b407e5a4a8d4a80
Alejandro Monge <alemonge2000@hotmail.com>
2025-05-05 16:13:06 -0600

    chore(operators): add hotel exercise

---
+++ fundamentals/operators/hotel.js
@@ -0,0 +1,30 @@
+/* 
+Crea un programa en JavaSCript que calcule la factura total de una persona en un hotel, donde cada habitación tiene un 
+precio de $100 por noche, aprovechando una oferta promocional que incluye un descuento del 5%.
+
+EL programa debe solicitar al huésped el número de noches que pasó en el hotel para calcular la factura final. 
+
+Si el usuario inhgresa que se quedo2 5 noches, la salida será: $475.00.
+*/
+
+const prompt = require('prompt-sync')()
+
+const PRICE_PER_DAY = 100
+
+const promoDiscount = 0.05
+
+const days = parseInt(prompt('Cuantos días desea quedarse? ->'))
+
+const subtotal = PRICE_PER_DAY * days
+const discount = subtotal * promoDiscount
+const finalPrice = subtotal - discount
+
+console.log(`
+    Hotel Receipt 
+    Nights: ${days}\n
+    Payment:
+     - Subtotal = $${subtotal}\n
+     -Discount = $${discount}\n
+     -Final Price = $${finalPrice}\n
+
+`)
